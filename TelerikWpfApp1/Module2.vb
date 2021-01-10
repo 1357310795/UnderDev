dc255588ca1b6dc227c144f14ce48bef922f0f68
1357310795 <35869804+1357310795@users.noreply.github.com>
2021-01-10 22:23:48 +0800

    Update Module2.vb
    111

--- TelerikWpfApp1/Module2.vb
+++ TelerikWpfApp1/Module2.vb
@@ -4,6 +4,14 @@ Imports System.Threading
 Imports System.Windows.Media.Animation
 
 Module Module2
+    Public Enum Edit_Mode_Enum As Integer
+        Cursor = 1
+        Pen = 2
+        Marker = 4
+        Eraser = 8
+        Selectt = 16
+    End Enum
+
     Public Sub SaveFrameworkElementToImage(ByVal ui As FrameworkElement, ByVal filepath As String)
         Try
             Dim ms As FileStream = New FileStream(filepath, FileMode.Create)
